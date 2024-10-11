cbff48d39df26f8796f800ba41df7d7c1559840c
Di Ta <xditaba@gmail.com>
2024-10-11 22:11:35 +0700

    Configuring jwt

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "lh3.googleusercontent.com",
+      "images.unsplash.com",
+      "avatars.githubusercontent.com",
+    ],
+  },
+};
 
 export default nextConfig;
