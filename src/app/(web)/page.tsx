76bc68eaf2ca11bfe645a0d35d32ef547f51f77c
Di Ta <xditaba@gmail.com>
2024-10-14 10:44:32 +0700

    Gallery Component

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,11 +1,15 @@
 import HeroSection from "@/components/HeroSection/HeroSection";
 import PageSearch from "@/components/PageSearch/PageSearch";
+import Gallery from "@/components/Gallery/Gallery";
 
 const Home = () => {
   return (
     <>
       <HeroSection />
       <PageSearch />
+      {/* FeaturedRoom */}
+      <Gallery />
+      {/* NewsLetter */}
     </>
   );
 };
